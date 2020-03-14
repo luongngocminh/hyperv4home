5f1f784f5f428f19d3123caa59350db10ec7a9d5
Ne-x <ngocminhluong182@gmail.com>
2020-03-14 11:16:35 +0700

    update bat

--- hyperinstalller.bat
+++ hyperinstalller.bat
@@ -1,6 +1,5 @@
 :: Hyper-V installation
 @echo off
-echo >>> Installing Hyper-V 
 dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum>hyper.txt
 set "prefix=dism /online /norestart /add-package:^"%SystemRoot%\servicing\Packages\"
 for /F "tokens=*" %%A in (hyper.txt) do (
@@ -8,10 +7,8 @@ for /F "tokens=*" %%A in (hyper.txt) do (
 )
 echo N | dism /online /enable-feature /featurename:Microsoft-Hyper-V -All /LimitAccess /ALL
 :: Containers installation
-echo >>> Installing Containers
 dir /b %SystemRoot%\servicing\Packages\*containers*.mum>containers.txt
-set "prefix=dism /online /norestart /add-package:^"%SystemRoot%\servicing\Packages\"
-for /F "tokens=*" %%A in (hyper.txt) do (
+for /F "tokens=*" %%A in (containers.txt) do (
     %prefix%%%A^"
 )
 echo N | dism /online /enable-feature /featurename:Containers -All /LimitAccess /ALL
